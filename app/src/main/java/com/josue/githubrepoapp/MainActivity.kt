6b548547df0f09f67a92c75916dd27e4ef63aa6b
josuedilmo <josuedilmo@gmail.com>
2022-03-31 00:37:30 +0100

    log info appears ALL info from Github API

--- app/src/main/java/com/josue/githubrepoapp/MainActivity.kt
+++ app/src/main/java/com/josue/githubrepoapp/MainActivity.kt
@@ -6,14 +6,18 @@ import android.os.Handler
 import android.os.Looper
 import android.service.autofill.UserData
 import android.util.Log
+import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.gson.Gson
+import com.google.gson.internal.`$Gson$Types`.arrayOf
 import kotlinx.android.synthetic.main.activity_main.*
 import okhttp3.*
 import java.io.IOException
+
+
 
 // APP IS USING GITHUB API
 class MainActivity : AppCompatActivity() {
@@ -21,7 +25,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
+        //start okHttpClient
         val client = OkHttpClient()
         val request = Request.Builder().url("https://api.github.com/users/Torvalds").build()
         client.newCall(request).enqueue(object: Callback {
@@ -32,13 +36,16 @@ class MainActivity : AppCompatActivity() {
             override fun onResponse(call: Call, response: Response) {
                 if (response.isSuccessful) {
                     val bodyString = response.body?.string()
-                    Log.i("INFO","msg: $bodyString")
-
+                    val json = Gson()
+                    val userGitData = json.fromJson(bodyString, UserGitData::class.java)
+                    Log.i("info","msg: $userGitData")
                 }
             }
-        })
+        })//finish okHttpClient
 
-    }
-}
+    }//finish fun onCreate
+    
 
 
+
+}//finish Class MainActivity
--- app/src/main/java/com/josue/githubrepoapp/UserGitData.kt
+++ app/src/main/java/com/josue/githubrepoapp/UserGitData.kt
@@ -1,11 +1,13 @@
 package com.josue.githubrepoapp
 
+import com.google.gson.annotations.SerializedName
+
 data class UserGitData(
-    val login: String,
-    val avatar_url: String,
-    val name: String,
-    val company: String,
-    val location: String,
-    val followers: Int,
-    val following: Int
+    @SerializedName("login") val login: String,
+    @SerializedName("avatar_url")val avatar_url: String,
+    @SerializedName("name")val name: String,
+    @SerializedName("company")val company: String,
+    @SerializedName("location")val location: String,
+    @SerializedName("followers")val followers: Int,
+    @SerializedName("following")val following: Int
 )
