4ce5dbf415e49d74eea65ba1d720a18193e16a2b
josuedilmo <josuedilmo@gmail.com>
2022-04-03 17:55:46 +0100

    Layouts done app running

--- app/src/main/java/com/josue/githubrepoapp/MainAdapter.kt
+++ app/src/main/java/com/josue/githubrepoapp/MainAdapter.kt
@@ -1,12 +1,15 @@
 package com.josue.githubrepoapp
 
 import android.content.Context
-import android.opengl.Visibility
+import android.icu.text.NumberFormat
+import android.os.Build
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.annotation.RequiresApi
 import androidx.recyclerview.widget.RecyclerView
+import java.util.*
 
 class MainAdapter (private val userRepoData: UserRepoGitData , private val context: Context): RecyclerView.Adapter<UserDataViewHolder>(){
 
@@ -16,16 +19,23 @@ class MainAdapter (private val userRepoData: UserRepoGitData , private val conte
         return UserDataViewHolder(view)
     }
 
+    @RequiresApi(Build.VERSION_CODES.N)
     override fun onBindViewHolder(holder: UserDataViewHolder, position: Int) {
         val data = userRepoData[position]
         holder.itemView.findViewById<TextView>(R.id.repoName).text = data.name
         holder.itemView.findViewById<TextView>(R.id.repoDescription).text = data.description
-        holder.itemView.findViewById<TextView>(R.id.repoVisible).text = data.visibility
+        holder.itemView.findViewById<TextView>(R.id.repoVisible).text = data.visibility.replaceFirstChar {
+            if (it.isLowerCase()) it.titlecase(
+                Locale.getDefault()
+            ) else it.toString()
+        }
         holder.itemView.findViewById<TextView>(R.id.repoLanguage).text = data.language
-        holder.itemView.findViewById<TextView>(R.id.repoStared).text = data.forks_count.toString()
-        holder.itemView.findViewById<TextView>(R.id.repoFork).text = data.stargazers_count.toString()
-        holder.itemView
-
+        //formatted starts count
+        val formatStarts = NumberFormat.getInstance().format(data.stargazers_count)
+        holder.itemView.findViewById<TextView>(R.id.repoStars).text = formatStarts
+        //formatted forks count
+        val formatForks = NumberFormat.getInstance().format(data.forks_count)
+        holder.itemView.findViewById<TextView>(R.id.repoForks).text = formatForks
     }
 
     override fun getItemCount(): Int {
--- app/src/main/java/com/josue/githubrepoapp/UserGitData.kt
+++ app/src/main/java/com/josue/githubrepoapp/UserGitData.kt
@@ -1,14 +1,12 @@
 package com.josue.githubrepoapp
 
-import com.google.gson.annotations.SerializedName
 
 data class UserGitData(
-    @SerializedName("login") val login: String,
-    @SerializedName("avatar_url")val avatar_url: String,
-    @SerializedName("name")val name: String,
-    @SerializedName("company")val company: String,
-    @SerializedName("location")val location: String,
-    @SerializedName("followers")val followers: Int,
-    @SerializedName("following")val following: Int,
-
+   val login: String,
+   val avatar_url: String,
+   val name: String,
+   val company: String,
+   val location: String,
+   val followers: Int,
+   val following: Int,
 )
